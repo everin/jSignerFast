cae63e7d687f98878d5e2e623b3289a6ea2e65e2
Everin Orlandi @W <everin.orlandi@gmail.com>
2015-11-10 17:17:06 +0100

    tests

---
+++ src/test/java/it/everin/ui/jSignerFastTest.java
@@ -0,0 +1,25 @@
+package it.everin.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class jSignerFastTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		System.out.println("ciao");
+		//fail("Not yet implemented");
+	}
+
+}
